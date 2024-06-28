556cef15ae349d8bc97a814f41a7855e63033d58
lela-akiashvili <l.akiashvili.doit@itstep.ge>
2024-06-29 02:50:49 +0400

    respnsivnes of a dead fish

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  private fb = inject(FormBuilder);
+  emailForm = this.fb.group({
+    email: ['', Validators.email],
+    subject: ['', Validators.required],
+    message: ['', Validators.required],
+  });
+  get controls(){
+    return this.emailForm.controls;
+  }
+  contact: boolean = false;
+  onContact() {
+    this.contact = !this.contact;
+    this.emailForm.reset();
+  }
 }
